80ed63586bc2370327ae3d39b26c0293659f18c6
Ashwani kumar <rathiashwani8@gmail.com>
2023-09-08 20:08:29 +0530

    set up

---
+++ src/utils.py
@@ -0,0 +1,41 @@
+import os
+import sys
+from src.exception import CustomException
+from src.logger import logging
+import pandas as pd
+from dotenv import load_dotenv, find_dotenv
+import pymysql
+# import mysql.connector
+
+load_dotenv(find_dotenv())
+
+host=os.environ.get("host")
+user=os.environ.get("user")
+password=os.environ.get("password")
+db=os.environ.get('db')
+
+
+
+def read_sql_data():
+    logging.info("Reading SQL database started")
+    try:
+        mydb=pymysql.connect(
+            host=host,
+            user=user,
+            password=password,
+            db=db
+            # port = 3306
+        )
+        logging.info("Connection Established",mydb)
+        df=pd.read_sql_query('Select * from students',mydb)
+        print(df.head())
+
+        return df
+
+
+
+    except Exception as e:
+        raise CustomException(e,sys)
+
+if __name__=="__main__":
+    obj = read_sql_data()
